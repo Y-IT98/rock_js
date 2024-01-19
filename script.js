08815d4d78d36556564e84b6fbd6dc9ce08004da
Youssef <youssef_nassar98@outlook.com>
2024-01-18 20:22:03 -0800

    Created another function that prompts the player for their choice, case insensitive, then stores the answer in a variable to be called later from the function when the game starts.

--- script.js
+++ script.js
@@ -7,12 +7,20 @@ var options = ['rock','paper', 'scissors']
 //get the random selection then assign it to a variable
 let get_Choice = Math.floor(Math.random()*options.length);
 let result = options[get_Choice];
-console.log(result);
+return(result);
      
  
 
 }
-getComputerChoice()
+const computer_selection = getComputerChoice()
+//function that asks the player for their choice and then stores it in a variable
+function get_player_choice(){
+    let choice = prompt("Rock, Paper , Scissors  ?").toLowerCase
+    return choice
 
+}
 
-//
+//Function that plays a round of the actual game
+//Takes player selection and computer selection as the parameters
+//After choices have been entered it then compares the two choices and declares a winner 
+// Boolean to compare results rocks beats scissors etc
