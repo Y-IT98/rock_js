0c15ae5c14bb7b7ccfb63e9a8c0f6dc7185239b2
Youssef <youssef_nassar98@outlook.com>
2024-01-24 09:46:07 -0800

    Fixed the code causing the win/loss/tie conditions to not evaluate properly, the wrong comparison operator was being used. Now working on creating a function to play a best of 5 game and track score.

--- script.js
+++ script.js
@@ -3,24 +3,57 @@
 function getComputerChoice(){
     //randomly returns rock paper or scissors and stores it in a variable 
 //create  an array  that stores the computers random selection 
-var options = ['rock','paper', 'scissors']
+    var options = ['rock','paper', 'scissors']
 //get the random selection then assign it to a variable
-let get_Choice = Math.floor(Math.random()*options.length);
-let result = options[get_Choice];
-return(result);
+    var get_Choice = Math.floor(Math.random()*options.length);
+    var result = options[get_Choice].toLowerCase();
+    return result;
      
  
 
 }
-const computer_selection = getComputerChoice()
+
+ var computer_selection = getComputerChoice()
 //function that asks the player for their choice and then stores it in a variable
 function get_player_choice(){
-    let choice = prompt("Rock, Paper , Scissors  ?").toLowerCase
-    return choice
+    let choice = prompt("Rock, Paper , Scissors  ?")
+    var lower_case_Choice = choice.toLowerCase()
+    return lower_case_Choice;
 
 }
+var player_Selection = get_player_choice()
 
-//Function that plays a round of the actual game
-//Takes player selection and computer selection as the parameters
-//After choices have been entered it then compares the two choices and declares a winner 
-// Boolean to compare results rocks beats scissors etc
+function playRound(player_Selection,computer_selection){
+    let player_Score = 0
+    let computer_Score = 0
+    // Plays a single round of RPS and identifies winning/losing conditions
+    //win 
+
+    // issue is code runs but the winning / loss /tie conditions arent being evaluated 
+    // code skips to the else block without evaluating if statements
+    if(player_Selection === computer_selection){
+        return "Its a tie "
+    } else if(player_Selection === "rock" && computer_selection === "scissors " ||
+              player_Selection === "paper" && computer_selection === "rock" || 
+              player_Selection === "scissors" && computer_selection === "paper"){
+        player_Score++
+        console.log(`player wins player: ${player_Score}- computer score: ${computer_Score}`)
+        return true
+    } else{
+        computer_Score++
+        console.log(`player lost score is player: ${player_Score}- computer score: ${computer_Score}`)
+        return false
+    }
+}
+            
+        
+    
+    
+        
+    
+
+console.log(playRound(player_Selection,computer_selection))
+playRound(player_Selection,computer_selection)
+playRound(player_Selection,computer_selection)
+playRound(player_Selection,computer_selection)
+playRound(player_Selection,computer_selection)
