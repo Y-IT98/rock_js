c8aa336140df9804da02e56226e81cdc9208ff85
Youssef <youssef_nassar98@outlook.com>
2024-03-07 20:52:05 -0800

    Created a function to keep score of both players and track the round count, once 5 rounds have been reached a winner is announced. The keep score function also acts as a means to disable the buttons from being clicked once the winner is announced. The other feature added to the game was the ability to restart after the round limit has been reached via a reset button which refreshes the window once clicked.

--- script.js
+++ script.js
@@ -14,80 +14,163 @@ function getComputerChoice(){
 
 }
 
- var computer_selection = getComputerChoice()
+ const computer_selection = getComputerChoice()
 //function that asks the player for their choice and then stores it in a variable
- let playerSelectionButton = document.querySelectorAll('button')
- playerSelectionButton.forEach((button)=>{
-    button.addEventListener('click',()=>{
-        player_Selection = button.id
-        computer_selection = getComputerChoice()
-        playRound(player_Selection,computer_selection)
-        
-    }
- 
- )}
- )  
- 
 
 
+
+
+
+let compScore = 0
+let playerScore = 0
+let roundsPlayed = 0
 
 function playRound(player_Selection, computer_selection) {
-    let resultText = document.querySelector('.Score')
-    //Function that has the win/loss conditions for player and computer and compares their inputs 
-    if (player_Selection === computer_selection) {
+    let resultText = document.querySelector('.Score');
+    let playerWin = document.querySelector('.player');
+    let computerWin = document.querySelector('.computer');
+    
 
-        return resultText.textContent="Its a Tie !";
+
+    //Function that has the win/loss conditions for player and computer and compares their inputs 
+   
+   
+        
+    
+
+     if (player_Selection === computer_selection) {
+        
+        resultText.textContent="Its a Tie !";
+        
     } else if (
         (player_Selection === "rock" && computer_selection === "scissors") ||
         (player_Selection === "paper" && computer_selection === "rock") ||
         (player_Selection === "scissors" && computer_selection === "paper")
-    ) {
-        return resultText.textContent= "You win!";
+        
+    ) { playerScore++
+        roundsPlayed++
+        playerWin.textContent = playerScore
+         resultText.textContent= "You win!"
+         keepScore();
     } else {
-        return resultText.textContent= "Computer Wins!";
+        roundsPlayed++
+        compScore++
+        computerWin.textContent = compScore
+        resultText.textContent= "Computer Wins!"
+        keepScore();
+   
+    }
+        
+    
+}
+function playerChoice(){
+    let playerButtonClick = document.querySelectorAll('button')
+    const gameButtons = document.getElementsByClassName('gameChoices')
+    let player_Selection;
+    playerButtonClick.forEach((button)=>{
+        button.addEventListener('click',()=>{
+            player_Selection = button.id
+            console.log(player_Selection)
+            let computer_selection = getComputerChoice()
+            playRound(player_Selection,computer_selection)
+           
+
+        })
+     
+            if (roundsPlayed === 5) {
+                gameButtons.forEach(button => {
+                    button.disabled = true; // Disable the button
+                });
+            }
+            
+ 
+        }
+       
+    
+    )}
+
+playerChoice()
+
+function keepScore(){
+    const gameButtons = document.querySelectorAll('.gameChoices')
+    
+    const whoWon = document.querySelector('.Winner')
+    
+        if ((playerScore>compScore) && (roundsPlayed === 5)) {
+            whoWon.textContent = "The player won the series, Game over";
+            gameButtons.forEach(button =>{
+                button.disabled = true;
+            })
+
+            
+            
+        } else if((compScore > playerScore) && (roundsPlayed === 5)){
+            whoWon.textContent = "The computer won the series, Game over";
+            gameButtons.forEach(button =>{
+                button.disabled = true;
+            })
+            
+      
     }
 }
+
+function Restartgame(){
+    const restartBtn = document.querySelector('.reset')
+    // const resultText = document.querySelector('.Score')
+    // const playerWin = document.querySelector('.Player')
+    // const computerWin = document.querySelector(".Computer")
+    restartBtn.addEventListener('click',()=>{
+        location.reload()
+
+    })
+    
+}
+Restartgame()
+
 // function games_Played(){
 //     //2 variables to keep track of player and computer scores 
 //     let Comp_wins = 0
 //     let player_wins = 0
 //     //loop to go infinitely until the condition is reached 
 //     //The condition being the player or computer reaching a score of 3 
+  
 //     while (true) {
-//         var computer_selection = getComputerChoice();
-//         var player_Selection = get_player_choice();
-//         var result = playRound(player_Selection, computer_selection);
-//         console.log(result);
+        
+//         let playerCard = document.querySelector('.player')
+//         let computerCard = document.querySelector('.computer')
+//         let resultCard = document.querySelector('.Score')
+//         // let computer_selection = getComputerChoice();
+//         let player_Selection = getPlayerSelection()
+        
+//         let result = playRound(player_Selection, computer_selection);
+//         resultCard.textContent = `${result}`;
 //         //use string method to check the output of the function to decide who won
 //         if(result.includes("tie")){
-//             console.log("its a tie lets play another round")
-//         } else if( result.includes("Player")){
-//             player_wins++
-//         } else{
-//             Comp_wins++
-//         }
-//         console.log(`Player score is ${player_wins}. Computer score is ${Comp_wins}`)
-//         // condition to break out of the loop using score 
-//         if (player_wins === 3) {
-//             console.log("player wins the game !")
-//             break
+//             resultCard.textContent = "its a tie lets play another round"
             
-//         } else if (Comp_wins === 3){
-//             console.log("Computer wins the game")
+//         } else if( result.includes("Player")){
+//            playerCard.textContent =` Player: ${player_wins++}`
+
+//         } else{
+//             computerCard.textContent = ` Computer: ${Comp_wins++}`
+//         }
+//         resultCard.textContent= `Player score is ${player_wins}. Computer score is ${Comp_wins}`
+//         // condition to break out of the loop using score 
+//         if (player_wins === 5) {
+//             resultCard.textContent = "player wins the game !"
+            
+//         } else if (Comp_wins === 5){
+//             resultCard.textContent = "Computer wins the game"
 //             break 
 //         }
 //     }
-    
+// }
 
   
-// }
-// // games_Played()
+ 
 
-// function clickButtons (playRound) {
-   
 
-// }
 
     
-
+// games_Played()
 
